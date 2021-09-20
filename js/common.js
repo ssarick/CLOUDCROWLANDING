0fd48d214b6c6efdf4c275afc5425c3088f6b53d
ssarick <66787805+ssarick@users.noreply.github.com>
2021-09-20 16:12:13 +0500

    new lang added

--- js/common.js
+++ js/common.js
@@ -74,7 +74,7 @@ var app = new Vue({
     el: '#app',
     data: {
         selected: {
-            id:'1',
+            id: '1',
             image: '../img/EDO-ill.svg',
             desc: '',
             link: ''
@@ -82,182 +82,225 @@ var app = new Vue({
         images: [
             {
                 src: ('../img/EDO-ill.svg'),
-                id:'1',
+                id: '1',
                 name: {
-                    ru:'ЭДО',
-                    en:'EDO',
-                    uz:'EDO',
+                    ru: 'ЭДО',
+                    en: 'EDO',
+                    uz: 'EDO',
+                    uzCyr: 'EДО'
                 },
                 link: {
                     ru: './pages/service.html#edo',
                     en: 'serviceEn.html#edo',
                     uz: 'serviceUz.html#edo',
+                    uzCyr: 'serviceUzCyr.html#edo'
                 },
                 text: {
                     ru: 'ЭДО - позволит вам легко и быстро обмениваться электронными документами',
                     en: 'hey',
-                    uz: ''
+                    uz: '',
+                    uzCyr: ''
                 }
             },
             {
                 src: ('../img/bank-ill.svg'),
-                id:'2',
+                id: '2',
 
                 name: {
-                    ru:'Банк',
-                    en:'Bank',
-                    uz:'Bank',
+                    ru: 'Банк',
+                    en: 'Bank',
+                    uz: 'Bank',
+                    uzCyr: 'Банк'
                 },
                 link: {
                     ru: './pages/service.html#bank',
                     en: 'serviceEn.html#bank',
                     uz: 'serviceUz.html#bank',
+                    uzCyr: 'serviceUzCyr.html#bank'
                 },
                 text: {
                     ru: 'Банк - позволит вам легко и быстро обмениваться электронными документами',
                     en: 'hey',
-                    uz: ''
+                    uz: '',
+                    uzCyr: ''
                 }
             },
             {
-                id:'3',
+                id: '3',
                 src: ('../img/storage-ill.svg'),
                 name: {
-                    ru:'Cклад',
-                    en:'Storage',
-                    uz:'Storage',
+                    ru: 'Cклад',
+                    en: 'Storage',
+                    uz: 'E-Ombor',
+                    uzCyr: 'E-Омбор'
                 },
                 link: {
                     ru: './pages/service.html#storage',
                     en: 'serviceEn.html#storage',
                     uz: 'serviceUz.html#storage',
+                    uzCyr: 'serviceUzCyr.html#storage'
+
                 },
                 text: {
                     ru: 'Банк - позволит вам легко и быстро обмениваться электронными документами',
                     en: 'hey',
-                    uz: ''
+                    uz: '',
+                    uzCyr: ''
                 }
             },
             {
-                id:'4',
+                id: '4',
                 src: ('../img/hr-ill.svg'),
                 name: {
-                    ru:'Кадры',
-                    en:'HR',
-                    uz:'HR',
+                    ru: 'Кадры',
+                    en: 'HR',
+                    uz: 'HR',
+                    uzCyr: 'HR'
                 },
                 link: {
                     ru: './pages/service.html#hr',
                     en: 'serviceEn.html#hr',
                     uz: 'serviceUz.html#hr',
+                    uzCyr: 'serviceUzCyr.html#hr'
+
                 },
                 text: {
                     ru: 'Банк - позволит вам легко и быстро обмениваться электронными документами',
                     en: 'hey',
-                    uz: ''
+                    uz: '',
+                    uzCyr: ''
                 }
             },
             {
-                id:'5',
+                id: '5',
                 src: ('../img/accounting-ill.svg'),
                 name: {
-                    ru:'Бухгалтерия',
-                    en:'Accounting',
-                    uz:'Buxgalteriya',
+                    ru: 'Бухгалтерия',
+                    en: 'Accounting',
+                    uz: 'Buxgalteriya',
+                    uzCyr: 'Бухгалтерия'
                 },
                 link: {
                     ru: './pages/service.html#accounting',
                     en: 'serviceEn.html#accounting',
                     uz: 'serviceUz.html#accounting',
+                    uzCyr: 'serviceUzCyr.html#accounting'
+
                 },
                 text: {
                     ru: 'Банк - позволит вам легко и быстро обмениваться электронными документами',
                     en: 'hey',
-                    uz: ''
+                    uz: '',
+                    uzCyr: ''
                 }
             },
             {
-                id:'6',
+                id: '6',
                 src: ('../img/statistics-ill.svg'),
                 name: {
-                    ru:'Статистика',
-                    en:'Statistics',
-                    uz:'Statistika',
+                    ru: 'Статистика',
+                    en: 'Statistics',
+                    uz: 'Statistika',
+                    uzCyr: 'Статистика'
                 },
                 link: {
                     ru: './pages/service.html#statistics',
                     en: 'serviceEn.html#statistics',
                     uz: 'serviceUz.html#statistics',
+                    uzCyr: 'serviceUzCyr.html#statistics'
+
                 },
                 text: {
                     ru: 'Банк - позволит вам легко и быстро обмениваться электронными документами',
                     en: 'hey',
-                    uz: ''
+                    uz: '',
+                    uzCyr: ''
                 }
             },
             {
-                id:'7',
+                id: '7',
                 src: ('../img/soliq-ill.svg'),
                 name: {
-                    ru:'Налоги',
-                    en:'Taxes',
-                    uz:'Soliq',
+                    ru: 'Налоги',
+                    en: 'Taxes',
+                    uz: 'Soliq',
+                    uzCyr: 'Солиқ'
                 },
                 link: {
                     ru: './pages/service.html#soliq',
                     en: 'serviceEn.html#soliq',
                     uz: 'serviceUz.html#soliq',
+                    uzCyr: 'serviceUzCyr.html#soliq'
+
                 },
                 text: {
                     ru: 'Банк - позволит вам легко и быстро обмениваться электронными документами',
                     en: 'hey',
-                    uz: ''
+                    uz: '',
+                    uzCyr: ''
                 }
             },
             {
-                id:'8',
+                id: '8',
                 src: ('../img/1c-ill.svg'),
                 name: {
-                    ru:'1C Интеграция',
-                    en:'1C Integration',
-                    uz:'1С Integratsiya',
+                    ru: '1C Интеграция',
+                    en: '1C Integration',
+                    uz: '1С Integratsiya',
+                    uzCyr: '1C Интеграция'
                 },
                 link: {
                     ru: './pages/service.html#1c',
                     en: 'serviceEn.html#1c',
                     uz: 'serviceUz.html#1c',
+                    uzCyr: 'serviceUzCyr.html#1c'
+
                 },
                 text: {
                     ru: 'Банк - позволит вам легко и быстро обмениваться электронными документами',
                     en: 'hey',
-                    uz: ''
+                    uz: '',
+                    uzCyr: ''
                 }
             },
             {
-                id:'9',
+                id: '9',
                 src: ('../img/msfo-ill.svg'),
                 name: {
-                    ru:'МСФО',
-                    en:'MSFO',
-                    uz:'MSFO',
+                    ru: 'МСФО',
+                    en: 'MSFO',
+                    uz: 'MSFO',
+                    uzCyr: 'МСФО'
                 },
                 link: {
                     ru: './pages/service.html#msfo',
                     en: 'serviceEn.html#msfo',
                     uz: 'serviceUz.html#msfo',
+                    uzCyr: 'serviceUzCyr.html#msfo'
+
                 },
                 text: {
                     ru: 'Банк - позволит вам легко и быстро обмениваться электронными документами',
                     en: 'hey',
-                    uz: ''
+                    uz: '',
+                    uzCyr: ''
                 }
             }
         ],
     },
     methods: {
         updateImg: function (src, text, link, id) {
-            let active = document.getElementById(id);
-            active.classList.toggle("active-item");
+            let items = document.getElementsByClassName("provided-item");
+            let actives = document.getElementsByClassName('active-item');
+            for (i = 0; items.length > i; i++) {
+                items[i].onclick = function() {
+                    let currentActive = actives[0];
+                    if (currentActive)
+                        currentActive.classList.remove("active-item");
+                    if (currentActive !== this)
+                        this.classList.add("active-item");
+                };
+            }
             this.selected.id = id
             this.selected.image = src
             this.selected.desc = text
